1e1815f3ff6b267f4d24e86a4b412acde5b8bd05
b10207146 <b10207146@mail.ntust.edu.tw>
2017-05-05 22:06:52 +0800

    Lesson 13

---
+++ main.js
@@ -0,0 +1,12 @@
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+
+
+function draw(){
+	ctx.drawImage(bgImg,0,0);
+}
+
+
+setTimeout(draw,1000);
